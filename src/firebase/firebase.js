b30779e46449788fbc0d49126f01c22927260d5c
Kush-316 <124437733+Kush-316@users.noreply.github.com>
2024-07-14 18:22:32 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAg6M2Tzscr1cRa4HMzMme4X0QYq2CN8k4",
+  apiKey: "//",
   authDomain: "boxclonedd.firebaseapp.com",
   databaseURL: "https://boxclonedd-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "boxclonedd",
   storageBucket: "boxclonedd.appspot.com",
-  messagingSenderId: "1003851103298",
-  appId: "1:1003851103298:web:692a4f937aa39741c0c1db",
-  measurementId: "G-4598D9R70F"
+  messagingSenderId: "//",
+  appId: "//",
+  measurementId: "//"
 };
 
 const app = initializeApp(firebaseConfig);
